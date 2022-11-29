dc4fda17f2425fd0695379efd376441b28cf4859
MrBroo <broooooomr@gmail.com.com>
2022-11-29 19:57:30 +0500

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const POST = process.env.POST || 5000;
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
@@ -16,9 +19,7 @@ app.use("/auth", authRouter);
 
 const start = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://Ibrohim:leVel0105@cluster0.tnwmh6q.mongodb.net/?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(process.env.MONGODB_URL);
     app.listen(POST, () =>
       console.log(`Server has been started on port ${POST}...`)
     );
